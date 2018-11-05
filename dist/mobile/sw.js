f8e41859443cb74470a5f01badb7e3f0f3aef0d2
Julio Pozo <julio.pozo@healthtrustpg.com>
2018-11-05 09:55:18 -0600

    added sw.js

---
+++ dist/mobile/sw.js
@@ -0,0 +1,16 @@
+var cacheName = 'Barcode';
+var filesToCache = [];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+    const url = new URL(event.request.url);
+});
