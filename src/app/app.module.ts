789c091a899c5b8385ce913e188baf5c085875f0
Julio Pozo <julio.pozo@healthtrustpg.com>
2018-11-06 09:05:52 -0600

    Integrate barcode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { BarcodeComponent } from './barcode/barcode.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'barcode', component: BarcodeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HomeComponent,
+    BarcodeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
---
+++ src/app/barcode/barcode.component.ts
@@ -0,0 +1,135 @@
+import { Component, OnInit } from '@angular/core';
+declare var Quagga: any;
+
+@Component({
+  selector: 'app-barcode',
+  templateUrl: './barcode.component.html',
+  styleUrls: ['./barcode.component.css']
+})
+
+
+export class BarcodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  barcode : string = '';
+ 
+  startScanner() : void {
+    let settings = {
+      decoder: { readers: ["code_128_reader", "code_39_reader"] },
+      locate: true,
+      inputStream: { 
+        name: 'Live', 
+        type: 'LiveStream', 
+        target: document.querySelector('#video-player')
+      },
+      
+      locator: { patchSize: "medium" },
+    };
+
+    Quagga.init(settings, function(err) {
+      
+      if (err) {
+          console.log(err);
+          return
+      }
+      
+      startVideo();
+      Quagga.start();
+
+      Quagga.onDetected(function(result) {
+        document.getElementById('barcode-result').innerText = 'Code: ' +  result.codeResult.code;
+      });
+
+      Quagga.onProcessed(function(result) {
+        drawBoxes(result)
+      });
+    
+    });
+    
+  }
+
+  stopScanner() : void {
+    stopVideo();
+  }
+  
+
+}
+
+var video = null;
+
+function startVideo() {
+
+  if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+    alert('UserMedia not supported')
+    return;
+  }
+
+  var constraints = { 
+    audio: false,
+    video: { facingMode: "environment" }
+  };
+  
+  navigator.mediaDevices.getUserMedia(constraints)
+    .then(function(stream) {
+      video = document.getElementById('video-player') as HTMLVideoElement;
+      video.srcObject = stream;
+    })
+    .catch(function(err){
+      alert(err);
+      console.log(err)
+    })
+   
+}
+
+function stopVideo() {
+  
+  try {
+    Quagga.stop();
+  }
+  catch {
+    return; // Intentionally suppressed
+  }
+
+  if(video.srcObject) {
+    let tracks = video.srcObject.getTracks();
+    
+    if(tracks && tracks[0]) {
+      let track = tracks[0];
+      track.stop();
+    }
+  }
+  
+  video.srcObject = null;
+  var canvas = document.getElementById('scanner-canvas') as HTMLCanvasElement;
+  const context = canvas.getContext('2d');
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  document.getElementById('barcode-result').innerText = '';
+}
+
+function drawBoxes(result) {
+  var drawingCtx = Quagga.canvas.ctx.overlay,
+  drawingCanvas = Quagga.canvas.dom.overlay;
+
+  if (result) {
+    if (result.boxes) {
+        drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+        result.boxes.filter(function (box) {
+            return box !== result.box;
+        }).forEach(function (box) {
+            Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 4});
+        });
+    }
+
+    if (result.box) {
+        Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 4});
+    }
+
+    if (result.codeResult && result.codeResult.code) {
+        Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 6});
+    }
+  }
+}
