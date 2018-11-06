e357ff837283f79ee3274a486494857c22aa2a35
Julio Pozo <julio.pozo@healthtrustpg.com>
2018-11-06 13:33:45 -0600

    remove ios add to home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,25 +10,8 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
 
-    iosPopup();
+
 
   }
 
 }
-
-function iosPopup() {
-  // Detects if device is on iOS 
-  const isIos = () => {
-    const userAgent = window.navigator.userAgent.toLowerCase();
-    return /iphone|ipad|ipod/.test( userAgent );
-  }
-
-  // Detects if device is in standalone mode
-  const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
-
-  // Checks if should display install popup notification:
-  if (isIos() && !isInStandaloneMode()) {
-    this.setState({ showInstallMessage: true });
-  }
-
-} 
