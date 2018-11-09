716c5a375ddf317ccbb7de1c42d2402c628b916a
Julio Pozo <julio.pozo@healthtrustpg.com>
2018-11-09 07:25:28 -0600

    ios fix

--- dist/mobile/main.js
+++ dist/mobile/main.js
@@ -193,23 +193,6 @@ var BarcodeComponent = /** @class */ (function () {
         this.barcode = '';
     }
     BarcodeComponent.prototype.ngOnInit = function () {
-        var settings = {
-            decoder: { readers: ["code_128_reader", "code_39_reader"] },
-            locate: true,
-            inputStream: {
-                name: 'Live',
-                type: 'LiveStream',
-                target: document.querySelector('#video-player')
-            },
-            locator: { patchSize: "medium" },
-        };
-        Quagga.init(settings, function (err) {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            Quagga.start();
-        });
     };
     BarcodeComponent.prototype.startScanner = function () {
         startVideo();
@@ -234,6 +217,23 @@ function startVideo() {
         alert('UserMedia not supported');
         return;
     }
+    var settings = {
+        decoder: { readers: ["code_128_reader", "code_39_reader"] },
+        locate: true,
+        inputStream: {
+            name: 'Live',
+            type: 'LiveStream',
+            target: document.querySelector('#video-player')
+        },
+        locator: { patchSize: "medium" },
+    };
+    Quagga.init(settings, function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        Quagga.start();
+    });
     Quagga.onDetected(function (result) {
         console.log(result.codeResult.code);
         if (this.barcode != result.codeResult.code) {
@@ -277,13 +277,59 @@ function startVideo() {
         alert(err);
         console.log(err);
     });
+    /*
+          Quagga.onDetected(function(result) {
+    
+            console.log(result.codeResult.code);
+            if(this.barcode != result.codeResult.code) {
+              var ul = document.getElementById('barcode-result');
+              var li = document.createElement("li");
+              li.innerText = result.codeResult.code;
+              ul.appendChild(li);
+              //document.getElementById('barcode-result').innerText = 'Code: ' +  result.codeResult.code;
+            }
+            
+            this.barcode = result.codeResult.code;
+          });
+      
+          Quagga.onProcessed(function(result) {
+            
+            var drawingCtx = Quagga.canvas.ctx.overlay,
+            drawingCanvas = Quagga.canvas.dom.overlay;
+      
+            if (result) {
+              if (result.boxes) {
+                  drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+                  result.boxes.filter(function (box) {
+                      return box !== result.box;
+                  }).forEach(function (box) {
+                      Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 4});
+                  });
+              }
+      
+              if (result.box) {
+                  Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 4});
+              }
+      
+              if (result.codeResult && result.codeResult.code) {
+                  Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 6});
+              }
+            }
+      
+          });
+    
+        })
+        .catch(function(err){
+          alert(err);
+          console.log(err)
+        })*/
 }
 function stopVideo() {
     try {
         Quagga.stop();
     }
-    catch (_a) {
-        return; // Intentionally suppressed
+    catch (e) {
+        alert(e);
     }
     if (video.srcObject) {
         var tracks = video.srcObject.getTracks();
